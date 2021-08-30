79269d9df8ceaeb7635f700d58cff4cf910188d3
Andrew Giang <giang184@gmail.com>
2021-08-30 11:02:38 -0700

    complete

--- __tests__/scripts.test.js
+++ __tests__/scripts.test.js
@@ -9,7 +9,11 @@ describe('coinCounter', () => {
       expect(coinCounter(4.25)).toEqual({quarters: 1, dimes:0, nickels:0, pennies:0});
     });
 
-    test('should correct return 2 quarter if value is 5.50', () => {
-      expect(coinCounter(5.50)).toEqual({quarters: 1, dimes:0, nickels:0, pennies:0});
+    test('should correct return 2 quarters if value is 5.50', () => {
+      expect(coinCounter(5.50)).toEqual({quarters: 2, dimes:0, nickels:0, pennies:0});
+    });
+
+    test('should correct return 2 quarters, 1 dime, 1 nickel, 3 pennies if value is 34.68', () => {
+      expect(coinCounter(34.68)).toEqual({quarters: 2, dimes:1, nickels:1, pennies:3});
     });
 });
--- src/coinCounter.js
+++ src/coinCounter.js
@@ -9,12 +9,12 @@ export const coinCounter = (money, result = {quarters:0, dimes:0, nickels:0, pen
   if (cents === 0) {
     return result;
   } else if (cents >= 25) {
-    return coinCounter(100*(cents-25), {quarters: result.quarters+1, dimes: result.dimes, nickels: result.nickels, pennies: result.pennies});
+    return coinCounter((cents-25)/100, {quarters: result.quarters+1, dimes: result.dimes, nickels: result.nickels, pennies: result.pennies});
   } else if (cents >= 10) {
-    return coinCounter(100*(cents-10), {quarters: result.quarters, dimes: result.dimes+1, nickels: result.nickels, pennies: result.pennies});
+    return coinCounter((cents-10)/100, {quarters: result.quarters, dimes: result.dimes+1, nickels: result.nickels, pennies: result.pennies});
   } else if (cents >= 5) {
-    return coinCounter(100*(cents-5), {quarters: result.quarters, dimes: result.dimes, nickels: result.nickels +1, pennies: result.pennies});
+    return coinCounter((cents-5)/100, {quarters: result.quarters, dimes: result.dimes, nickels: result.nickels +1, pennies: result.pennies});
   } else {
-    return coinCounter(100*(cents-1), {quarters: result.quarters, dimes: result.dimes, nickels: result.nickels, pennies: result.pennies +1});
+    return coinCounter((cents-1)/100, {quarters: result.quarters, dimes: result.dimes, nickels: result.nickels, pennies: result.pennies +1});
   }
 }
